baf4935f888791889d23198656ad0ff32f6680da
RandomHashTags <imrandomhashtags@gmail.com>
2019-06-21 21:38:04 -0500

    Initial Commit

---
+++ src/main/java/me/randomhashtags/cosmicvaults/CosmicVaults.java
@@ -0,0 +1,338 @@
+package me.randomhashtags.cosmicvaults;
+
+import me.randomhashtags.cosmicvaults.utils.CVPlayer;
+import me.randomhashtags.cosmicvaults.utils.universal.UInventory;
+import me.randomhashtags.cosmicvaults.utils.universal.UMaterial;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+public final class CosmicVaults extends JavaPlugin implements CommandExecutor, Listener {
+
+    public static CosmicVaults getPlugin;
+    private String prefix, menuTitle;
+    private HashMap<Player, Integer> editing, editingName, editingIcon;
+    public ItemStack defaultPvsDisplay;
+
+    private UInventory editIcon;
+    private FileConfiguration config;
+
+    public void onEnable() {
+        getPlugin = this;
+        getCommand("playervault").setExecutor(this);
+        Bukkit.getPluginManager().registerEvents(this, this);
+        saveDefaultConfig();
+
+        config = getConfig();
+
+        prefix = ChatColor.translateAlternateColorCodes('&', config.getString("messages.prefix"));
+        editing = new HashMap<>();
+        editingName = new HashMap<>();
+        editingIcon = new HashMap<>();
+
+        defaultPvsDisplay = d(config, "CosmicVaults.default pvs display");
+        menuTitle = ChatColor.translateAlternateColorCodes('&', config.getString("CosmicVaults.titles.pvs"));
+
+        editIcon = new UInventory(null, config.getInt("edit icon.size"), ChatColor.translateAlternateColorCodes('&', config.getString("edit icon.title")));
+        final ConfigurationSection c = config.getConfigurationSection("edit icon");
+        final List<String> selectLore = new ArrayList<>();
+        for(String s : config.getStringList("edit icon.select lore")) {
+            selectLore.add(ChatColor.translateAlternateColorCodes('&', s));
+        }
+        if(c != null) {
+            final Inventory i = editIcon.getInventory();
+            for(String s : c.getKeys(false)) {
+                final ItemStack is = d(config, "edit icon." + s);
+                if(is != null) {
+                    final ItemMeta m = is.getItemMeta();
+                    final List<String> l = new ArrayList<>();
+                    if(m.hasLore()) l.addAll(m.getLore());
+                    l.addAll(selectLore);
+                    m.setLore(l);
+                    is.setItemMeta(m);
+                    i.setItem(Integer.parseInt(s), is);
+                }
+            }
+        }
+
+        for(Player p : Bukkit.getOnlinePlayers()) {
+            CVPlayer.get(p.getUniqueId()).load();
+        }
+    }
+
+    public void onDisable() {
+        for(Player p : new ArrayList<>(editing.keySet())) {
+            p.closeInventory();
+        }
+        for(Player p : new ArrayList<>(editingName.keySet())) {
+            p.closeInventory();
+        }
+        for(Player p : new ArrayList<>(editingIcon.keySet())) {
+            p.closeInventory();
+        }
+        CVPlayer.unloadAllPlayerData();
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        final Player player = sender instanceof Player ? (Player) sender : null;
+        if(player != null) {
+            if(args.length == 0) {
+                viewMenu(player);
+            } else {
+                final int v = getRemainingInt(args[0]);
+                if(v > 0 &&  v <= CVPlayer.get(player.getUniqueId()).getMaxVaultPerms()) {
+                    viewVault(player, v);
+                } else {
+                    final HashMap<String, String> replacements = new HashMap<>();
+                    replacements.put("{PREFIX}", prefix);
+                    replacements.put("{VAULT_NUMBER}", Integer.toString(v));
+                    sendStringListMessage(player, config.getStringList("messages.no vault access"), replacements);
+                }
+            }
+        }
+        return true;
+    }
+    public int getDefaultSizeOfVault() {
+        return config.getInt("CosmicVaults.size of vault");
+    }
+    public String getDefaultVaultTitle() {
+        return ChatColor.translateAlternateColorCodes('&', config.getString("CosmicVaults.titles.self"));
+    }
+    public void viewMenu(Player player) {
+        if(hasPermission(player, "CosmicVaults.playervaults")) {
+            player.closeInventory();
+            final CVPlayer pdata = CVPlayer.get(player.getUniqueId());
+            final HashMap<Integer, UInventory> vaults = pdata.getVaults();
+            final int s = vaults.size(), size = s > 54 ? 54 : s%9 == 0 && s > 0 ? s : ((s+9)/9)*9;
+            player.openInventory(Bukkit.createInventory(player, size, menuTitle));
+            final Inventory top = player.getOpenInventory().getTopInventory();
+            for(int i = 0; i < s && i < config.getInt("CosmicVaults.max size of pvs display"); i++) {
+                top.setItem(i, pdata.getDisplay(i+1));
+            }
+            player.updateInventory();
+        }
+    }
+    public void viewVault(Player player, int number) {
+        if(hasPermission(player, "CosmicVaults.pv." + number)) {
+            final CVPlayer vaults = CVPlayer.get(player.getUniqueId());
+            final UInventory vault = vaults.getVault(number);
+            player.closeInventory();
+            player.openInventory(vault.getInventory());
+            player.updateInventory();
+            editing.put(player, number);
+        }
+    }
+    public void saveVault(Player player, int vault, Inventory inventory) {
+        CVPlayer.get(player.getUniqueId()).getVaults().get(vault).getInventory().setContents(inventory.getContents());
+    }
+
+    private boolean hasPermission(CommandSender sender, String permission) {
+        return sender instanceof Player || sender.hasPermission(permission);
+    }
+    private void sendStringListMessage(CommandSender sender, List<String> message, HashMap<String, String> replacements) {
+        for(String s : message) {
+            if(replacements != null) {
+                for(String r : replacements.keySet()) s = s.replace(r, replacements.get(r));
+            }
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', s));
+        }
+    }
+    private int getRemainingInt(String string) {
+        string = ChatColor.stripColor(ChatColor.translateAlternateColorCodes('&', string)).replaceAll("\\p{L}", "").replaceAll("\\s", "").replaceAll("\\p{P}", "").replaceAll("\\p{S}", "");
+        return string.equals("") ? -1 : Integer.parseInt(string);
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    private void playerChatEvent(AsyncPlayerChatEvent event) {
+        final Player player = event.getPlayer();
+        if(!event.isCancelled() && editingName.containsKey(player)) {
+            event.setCancelled(true);
+            final String s = event.getMessage();
+            final int vault = editingName.get(player);
+            final HashMap<String, String> replacements = new HashMap<>();
+            replacements.put("{PREFIX}", prefix);
+            replacements.put("{VAULT_NUMBER}", Integer.toString(vault));
+            replacements.put("{NAME}", s);
+            if(s.equals("cancel")) {
+                sendStringListMessage(player, config.getStringList("messages.rename vault cancelled"), replacements);
+            } else {
+                CVPlayer.get(player.getUniqueId()).setDisplayName(vault, s);
+                sendStringListMessage(player, config.getStringList("messages.rename vault success"), replacements);
+            }
+            if(config.getBoolean("CosmicVaults.open menu when.rename vault")) {
+                viewMenu(player);
+            }
+            editingName.remove(player);
+        }
+    }
+    @EventHandler
+    private void playerJoinEvent(PlayerJoinEvent event) {
+        CVPlayer.get(event.getPlayer().getUniqueId()).load();
+    }
+    @EventHandler
+    private void playerQuitEvent(PlayerQuitEvent event) {
+        CVPlayer.get(event.getPlayer().getUniqueId()).unload();
+    }
+    @EventHandler(priority = EventPriority.HIGHEST)
+    private void inventoryCloseEvent(InventoryCloseEvent event) {
+        final Player player = (Player) event.getPlayer();
+        if(editing.containsKey(player)) {
+            saveVault(player, editing.get(player), event.getInventory());
+            editing.remove(player);
+            if(config.getBoolean("CosmicVaults.open menu when.closed self player vault")) {
+                Bukkit.getScheduler().scheduleSyncDelayedTask(this, () -> viewMenu(player), 1);
+            }
+        } else if(editingIcon.containsKey(player)) {
+            editingIcon.remove(player);
+            if(config.getBoolean("CosmicVaults.open menu when.closed edit icon")) {
+                Bukkit.getScheduler().scheduleSyncDelayedTask(this, () -> viewMenu(player), 1);
+            }
+        }
+    }
+    @EventHandler
+    private void inventoryClickEvent(InventoryClickEvent event) {
+        if(!event.isCancelled()) {
+            final Player player = (Player) event.getWhoClicked();
+            if(event.getView().getTitle().equals(menuTitle)) {
+                event.setCancelled(true);
+                player.updateInventory();
+                final int r = event.getRawSlot();
+                final ItemStack c = event.getCurrentItem();
+                if(r < 0 || r >= player.getOpenInventory().getTopInventory().getSize() || c == null || c.getType().equals(Material.AIR)) return;
+                final String a = event.getClick().name();
+                if(a.contains("LEFT")) {
+                    viewVault(player, r+1);
+                } else if(a.contains("RIGHT")) {
+                    editIcon(player, r+1);
+                } else if(a.contains("MIDDLE")) {
+                    enterEditName(player, r+1);
+                }
+                player.updateInventory();
+            } else if(editingIcon.containsKey(player)) {
+                event.setCancelled(true);
+                player.updateInventory();
+                final int r = event.getRawSlot(), vault = editingIcon.get(player);
+                final ItemStack c = event.getCurrentItem();
+                if(r < 0 || r >= player.getOpenInventory().getTopInventory().getSize() || c == null || c.getType().equals(Material.AIR)) return;
+                final UMaterial material = UMaterial.match(c);
+                CVPlayer.get(player.getUniqueId()).setIcon(vault, material);
+                player.closeInventory();
+                final HashMap<String, String> replacements = new HashMap<>();
+                replacements.put("{PREFIX}", prefix);
+                replacements.put("{VAULT_NUMBER}", Integer.toString(vault));
+                replacements.put("{MATERIAL}", material.name());
+                sendStringListMessage(player, config.getStringList("messages.save edited icon"), replacements);
+            }
+        }
+    }
+
+    public void enterEditName(Player player, int vault) {
+        if(!editingName.containsKey(player)) {
+            player.closeInventory();
+            editingName.put(player, vault);
+            final HashMap<String, String> replacements = new HashMap<>();
+            replacements.put("{PLAYER}", player.getName());
+            replacements.put("{VAULT_NUMBER}", Integer.toString(vault));
+            replacements.put("{PREFIX}", prefix);
+            sendStringListMessage(player, config.getStringList("messages.rename vault"), replacements);
+            player.updateInventory();
+        }
+    }
+    public void editIcon(Player player, int vault) {
+        if(!editingIcon.containsKey(player)) {
+            player.openInventory(Bukkit.createInventory(player, editIcon.getSize(), editIcon.getTitle().replace("{VAULT_NUMBER}", Integer.toString(vault))));
+            player.getOpenInventory().getTopInventory().setContents(editIcon.getInventory().getContents());
+            player.updateInventory();
+            editingIcon.put(player, vault);
+        }
+    }
+
+
+    public ItemStack d(FileConfiguration config, String path) {
+        ItemStack item = null;
+        ItemMeta itemMeta = null;
+        if(config == null && path != null || config != null && config.get(path + ".item") != null) {
+            final String PP = config == null ? path : config.getString(path + ".item");
+            String P = PP.toLowerCase();
+
+            int amount = config != null && config.get(path + ".amount") != null ? config.getInt(path + ".amount") : 1;
+            if(P.toLowerCase().contains(";amount=")) {
+                final Random random = new Random();
+                final String A = P.split("=")[1];
+                final boolean B = P.contains("-");
+                final int min = B ? Integer.parseInt(A.split("-")[0]) : 0;
+                amount = B ? min+random.nextInt(Integer.parseInt(A.split("-")[1])-min+1) : Integer.parseInt(A);
+                path = path.split(";amount=")[0];
+                P = P.split(";")[0];
+            }
+
+            boolean enchanted = config != null && config.getBoolean(path + ".enchanted");
+            SkullMeta m = null;
+            String name = config != null ? config.getString(path + ".name") : null;
+            final String[] material = P.toUpperCase().split(":");
+            final String mat = material[0];
+            final byte data = material.length == 2 ? Byte.parseByte(material[1]) : 0;
+            final UMaterial U = UMaterial.match(mat + (data != 0 ? ":" + data : ""));
+            try {
+                item = U.getItemStack();
+                final Material skullitem = UMaterial.PLAYER_HEAD_ITEM.getMaterial(), i = item.getType();
+                if(!i.equals(Material.AIR)) {
+                    item.setAmount(amount);
+                    itemMeta = item.getItemMeta();
+                    if(i.equals(skullitem)) {
+                        m = (SkullMeta) itemMeta;
+                        if(item.getData().getData() == 3) m.setOwner(P.split(":").length == 4 ? P.split(":")[3].split("}")[0] : "RandomHashTags");
+                    }
+                    (i.equals(skullitem) ? m : itemMeta).setDisplayName(name != null ? ChatColor.translateAlternateColorCodes('&', name) : null);
+
+                    if(enchanted) itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+                    final HashMap<Enchantment, Integer> enchants = new HashMap<>();
+                    List<String> lore = new ArrayList<>();
+                    if(config != null && config.get(path + ".lore") != null) {
+                        for(String string : config.getStringList(path + ".lore")) {
+                            lore.add(ChatColor.translateAlternateColorCodes('&', string));
+                        }
+                    }
+                    (!i.equals(skullitem) ? itemMeta : m).setLore(lore);
+                    item.setItemMeta(!item.getType().equals(skullitem) ? itemMeta : m);
+                    if(enchanted) item.addUnsafeEnchantment(Enchantment.ARROW_DAMAGE, 1);
+                    for(Enchantment enchantment : enchants.keySet()) {
+                        if(enchantment != null) {
+                            item.addUnsafeEnchantment(enchantment, enchants.get(enchantment));
+                        }
+                    }
+                }
+            } catch(Exception e) {
+                System.out.println("UMaterial null itemstack. mat=" + mat + ";data=" + data + ";versionName=" + (U != null ? U.getVersionName() : null) + ";getMaterial()=" + (U != null ? U.getMaterial() : null));
+                return null;
+            }
+        }
+        return item;
+    }
+}
---
+++ src/main/java/me/randomhashtags/cosmicvaults/utils/CVPlayer.java
@@ -0,0 +1,240 @@
+package me.randomhashtags.cosmicvaults.utils;
+
+import me.randomhashtags.cosmicvaults.CosmicVaults;
+import me.randomhashtags.cosmicvaults.utils.universal.UInventory;
+import me.randomhashtags.cosmicvaults.utils.universal.UMaterial;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class CVPlayer {
+    private static final String s = File.separator, folder = CosmicVaults.getPlugin.getDataFolder() + s + "_player data";
+    public static final HashMap<UUID, CVPlayer> players = new HashMap<>();
+    private static final CosmicVaults api = CosmicVaults.getPlugin;
+
+    private UUID uuid;
+    public File file = null;
+    private YamlConfiguration yml = null;
+
+    private boolean isLoaded = false;
+    private HashMap<Integer, UInventory> vaults;
+    private HashMap<Integer, ItemStack> vaultDisplay;
+
+    public CVPlayer(UUID uuid) {
+        this.uuid = uuid;
+        final File f = new File(folder, uuid.toString() + ".yml");
+        boolean backup = false;
+        if(!players.containsKey(uuid)) {
+            if(!f.exists()) {
+                try {
+                    final File folder = new File(CVPlayer.folder);
+                    if(!folder.exists()) {
+                        folder.mkdirs();
+                    }
+                    f.createNewFile();
+                    backup = true;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            file = new File(folder, uuid.toString() + ".yml");
+            yml = YamlConfiguration.loadConfiguration(file);
+            players.put(uuid, this);
+        }
+        if(backup) backup();
+    }
+    public static CVPlayer get(UUID player) { return players.getOrDefault(player, new CVPlayer(player)); }
+    public void backup() {
+        yml.set("name", Bukkit.getOfflinePlayer(uuid).getName());
+
+        yml.set("vaults", null);
+        final HashMap<Integer, UInventory> vaults = getVaults();
+        final HashMap<Integer, ItemStack> displays = getVaultDisplays();
+        for(int i : displays.keySet()) {
+            final ItemStack is = displays.get(i);
+            final ItemMeta meta = is.getItemMeta();
+            final UMaterial m = UMaterial.match(is);
+            yml.set("vaults." + i + ".display.item", m.name());
+            yml.set("vaults." + i + ".display.amount", is.getAmount());
+            if(meta.hasDisplayName()) yml.set("vaults." + i + ".display.name", meta.getDisplayName());
+            if(meta.hasLore()) yml.set("vaults." + i + ".display.lore", meta.getLore());
+        }
+        for(int i : vaults.keySet()) {
+            final UInventory u = vaults.get(i);
+            final Inventory inv = u.getInventory();
+            final int size = u.getSize();
+            yml.set("vaults." + i + ".size", size);
+            yml.set("vaults." + i + ".title", u.getTitle());
+            for(int a = 0; a < size; a++) {
+                final ItemStack is = inv.getItem(a);
+                if(is != null) {
+                    final UMaterial um = UMaterial.match(is);
+                    yml.set("vaults." + i + ".items." + a + ".item", um.name());
+                    yml.set("vaults." + i + ".items." + a + ".amount", is.getAmount());
+                    if(is.hasItemMeta()) {
+                        final ItemMeta m = is.getItemMeta();
+                        if(m.hasDisplayName()) yml.set("vaults." + i + ".items." + a + ".name", m.getDisplayName());
+                        if(m.hasLore()) yml.set("vaults." + i + ".items." + a + ".lore", m.getLore());
+                    }
+                }
+            }
+        }
+        save();
+    }
+    public CVPlayer load() {
+        if(!isLoaded) {
+            isLoaded = true;
+            return this;
+        }
+        return players.get(uuid);
+    }
+    public void unload() {
+        if(isLoaded) {
+            backup();
+            isLoaded = false;
+            file = null;
+            yml = null;
+            players.remove(uuid);
+            uuid = null;
+            vaults = null;
+            vaultDisplay = null;
+        }
+    }
+
+    public boolean isLoaded() { return isLoaded; }
+    public UUID getUUID() { return uuid; }
+    public YamlConfiguration getYaml() { return yml; }
+
+    public int getMaxVaultPerms() {
+        int a = 0;
+        final Player player = Bukkit.getPlayer(uuid);
+        for(int i = 1; i <= 100; i++) {
+            if(player.hasPermission("CosmicVaults.pv." + i)) {
+                a = i;
+            }
+        }
+        return a;
+    }
+    private void loadVaults() {
+        if(vaults == null) vaults = new HashMap<>();
+        final CosmicVaults cv = CosmicVaults.getPlugin;
+        final int max = getMaxVaultPerms(), def = cv.getDefaultSizeOfVault();
+        final String deft = cv.getDefaultVaultTitle();
+        final Player player = Bukkit.getPlayer(uuid);
+        for(int i = 1; i <= max; i++) {
+            if(!vaults.containsKey(i)) {
+                vaults.put(i, new UInventory(player, yml.getInt("vaults." + i + ".size", def), yml.getString("vaults." + i + ".title", deft.replace("{VAULT_NUMBER}", Integer.toString(i)))));
+                if(yml.get("vaults." + i + ".items") != null) {
+                    final ConfigurationSection c = yml.getConfigurationSection("vaults." + i + ".items");
+                    if(c != null) {
+                        final Inventory inv = vaults.get(i).getInventory();
+                        for(String s : c.getKeys(false)) {
+                            inv.setItem(Integer.parseInt(s), api.d(yml, "vaults." + i + ".items." + s));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public HashMap<Integer, UInventory> getVaults() {
+        loadVaults();
+        return vaults;
+    }
+    public UInventory getVault(int vault) {
+        loadVaults();
+        return vaults.getOrDefault(vault, null);
+    }
+    private void loadVaultDisplays() {
+        if(vaultDisplay == null) {
+            vaultDisplay = new HashMap<>();
+            final ItemStack is = api.defaultPvsDisplay;
+            final List<String> lore = new ArrayList<>();
+            for(int i = 1; i <= getMaxVaultPerms(); i++) {
+                if(yml.get("vaults." + i) != null) {
+                    vaultDisplay.put(i, api.d(yml, "vaults." + i + ".display"));
+                } else {
+                    final String v = Integer.toString(i);
+                    final ItemStack item = is.clone();
+                    item.setAmount(i);
+                    final ItemMeta meta = item.getItemMeta();
+                    lore.clear();
+                    meta.setDisplayName(meta.getDisplayName().replace("{VAULT_NUMBER}", v));
+                    for(String s : meta.getLore()) {
+                        lore.add(s.replace("{VAULT_NUMBER}", v));
+                    }
+                    meta.setLore(lore);
+                    item.setItemMeta(meta);
+                    vaultDisplay.put(i, item);
+                }
+            }
+        }
+    }
+    public HashMap<Integer, ItemStack> getVaultDisplays() {
+        loadVaultDisplays();
+        return vaultDisplay;
+    }
+    public ItemStack getDisplay(int vault) {
+        loadVaultDisplays();
+        return vaultDisplay.getOrDefault(vault, new ItemStack(Material.BARRIER));
+    }
+    public void setDisplayName(int vault, String name) {
+        final ItemStack is = getDisplay(vault);
+        final ItemMeta m = is.getItemMeta();
+        m.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
+        is.setItemMeta(m);
+    }
+    public void setIcon(int vault, UMaterial icon) {
+        final ItemStack is = getDisplay(vault), d = icon.getItemStack();
+        d.setItemMeta(is.getItemMeta());
+        vaultDisplay.put(vault, d);
+    }
+    public void setVaults(HashMap<Integer, UInventory> vaults) {
+        this.vaults = vaults;
+    }
+    public ItemStack[] getVaultItems(int vault) {
+        return vaults.containsKey(vault) ? vaults.get(vault).getInventory().getContents() : null;
+    }
+    public void setVaultItems(int vault, ItemStack[] items) {
+        if(!vaults.containsKey(vault)) vaults.put(vault, new UInventory(null, 54, "Vault #" + vault));
+        vaults.get(vault).getInventory().setContents(items);
+    }
+
+
+    private void save() {
+        try {
+            yml.save(file);
+            yml = YamlConfiguration.loadConfiguration(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public OfflinePlayer getOfflinePlayer() {
+        return uuid != null ? Bukkit.getOfflinePlayer(uuid) : null;
+    }
+
+    public static void loadAllPlayerData() {
+        try {
+            for(File f : new File(folder).listFiles()) {
+                CVPlayer.get(UUID.fromString(f.getName().split("\\.yml")[0])).load();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void unloadAllPlayerData() {
+        for(CVPlayer p : new ArrayList<>(players.values())) {
+            p.unload();
+        }
+    }
+}
